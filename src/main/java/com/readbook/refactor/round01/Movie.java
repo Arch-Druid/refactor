b23c74c73684405cbff903262c0845637de3f84c
yupeng <ypyupeng@icloud.com>
2016-11-16 18:16:15 +0800

    重构第三步 , 将rentals 里 switch movie方法挪到 movie 中 .

--- src/main/java/com/readbook/refactor/round01/Movie.java
+++ src/main/java/com/readbook/refactor/round01/Movie.java
@@ -22,4 +22,37 @@ public class Movie {
     }
 
 
+    public int getFrequentRenterPoints(int daysRented) {
+        int frequentRenterPoints = 0;
+        frequentRenterPoints++;
+        if ((get_priceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
+            frequentRenterPoints++;
+        }
+        return frequentRenterPoints;
+    }
+
+
+    public double getCharge(int daysRented) {
+        double thisAmount = 0;
+        switch (get_priceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (daysRented > 2) {
+                    thisAmount += (daysRented - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (daysRented > 3) {
+                    thisAmount += (daysRented - 3) * 1.5;
+                }
+                break;
+        }
+        return thisAmount;
+    }
+
+
 }
